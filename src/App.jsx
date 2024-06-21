8a87f3d61c45f4f1435fb80aa03675c14fa9f1fe
ChristopherDuggan <christopherduggan@gmail.com>
2024-06-21 11:25:12 -0400

    create pokemon list

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import './App.css'
+import { useState } from 'react';
+
+import './App.css';
+import PokemonList from './components/PokemonList';
 
 function App() {
+  
+  const initialState = [
+    { _id: 1, name: 'bulbasaur', weight: 69, height: 7 },
+    { _id: 2, name: 'ivysaur', weight: 130, height: 10 },
+    { _id: 3, name: 'venusaur', weight: 1000, height: 20 },
+    { _id: 4, name: 'charmander', weight: 85, height: 6 },
+    { _id: 5, name: 'charmeleon', weight: 190, height: 11 },
+  ]
+
+  const [pokemon, setPokemon] = useState(initialState);
+
   return (
-    <h1>hello</h1>
+    <>
+      <h1>Pokemon!</h1>
+      <PokemonList pokemon={pokemon} />
+    </>
   )
 }
 
